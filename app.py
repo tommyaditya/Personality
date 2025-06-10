eeb8ed3be79795430582a903b47062d2621b626e
tommyaditya <160828804+tommyaditya@users.noreply.github.com>
2025-06-11 04:26:50 +0700

    app.py

--- app.py
+++ app.py
@@ -2,17 +2,18 @@ import streamlit as st
 import pandas as pd
 import pickle
 
-# Load model
+# Load model dan encoder
 model = pickle.load(open("model.pkl", "rb"))
+le = pickle.load(open("encoder.pkl", "rb"))
 
 st.title("Prediksi Kepribadian")
 
 with st.form("form"):
-    time_alone = st.slider("Waktu dihabiskan sendiri (0-10)", 0, 10, 5)
-    event_attend = st.slider("Kehadiran di acara sosial (0-10)", 0, 10, 5)
-    going_out = st.slider("Frekuensi keluar rumah (0-10)", 0, 10, 5)
-    friend_circle = st.slider("Ukuran lingkaran pertemanan (0-10)", 0, 10, 5)
-    post_freq = st.slider("Frekuensi posting di medsos (0-10)", 0, 10, 5)
+    time_alone = st.slider("Waktu dihabiskan sendiri (0-10)", 0, 15, 5)
+    event_attend = st.slider("Kehadiran di acara sosial (0-10)", 0, 15, 5)
+    going_out = st.slider("Frekuensi keluar rumah (0-10)", 0, 15, 5)
+    friend_circle = st.slider("Ukuran lingkaran pertemanan (0-10)", 0, 15, 5)
+    post_freq = st.slider("Frekuensi posting di medsos (0-10)", 0, 15, 5)
     stage_fear = st.selectbox("Takut tampil di depan umum?", ["Yes", "No"])
     drained_social = st.selectbox("Lelah setelah bersosialisasi?", ["Yes", "No"])
     submit = st.form_submit_button("Prediksi")
@@ -29,4 +30,6 @@ if submit:
     })
 
     prediction = model.predict(input_df)[0]
-    st.success(f"Prediksi kepribadianmu: **{prediction}**")
+    personality = le.inverse_transform([prediction])[0]  # ← ubah ke label asli
+
+    st.success(f"Kamu kemungkinan adalah seorang: **{personality}**")
